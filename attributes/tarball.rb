ea2653c663f0fb2f37e8a9f6b64d2cf1259c0549
Michael S. Klishin <michael@novemberain.com>
2012-08-19 16:17:34 +0400

    A typo

--- attributes/tarball.rb
+++ attributes/tarball.rb
@@ -2,7 +2,7 @@ version = "3.1"
 
 default[:clang] = {
   :version => version,
-  :arch    => case node.clang.version
+  :arch    => case version
               when "3.1" then
                 kernel['machine'] =~ /x86_64/ ? "x86_64" : "x86"
               else
